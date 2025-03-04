4d0b2a3d4e1cde917ecba57ffbbea6e301e7aac5
aimitrofanova <aimitrofanova@mail.ru>
2025-03-04 16:17:23 +0600

    project

--- test_add_group.py
+++ test_add_group.py
@@ -2,10 +2,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
 from selenium.webdriver.firefox.service import Service
 
 class Group:
@@ -14,73 +10,96 @@ class Group:
         self.header = header
         self.footer = footer
 
+class SessionHelper:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def login(self, username, password):
+        self.open_home_page()
+        self.fill_login_form(username, password)
+        self.submit_login()
+
+    def logout(self):
+        self.driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def open_home_page(self):
+        self.driver.get("http://localhost/addressbook/")
+
+    def fill_login_form(self, username, password):
+        self.driver.find_element(By.NAME, "user").clear()
+        self.driver.find_element(By.NAME, "user").send_keys(username)
+        self.driver.find_element(By.NAME, "pass").clear()
+        self.driver.find_element(By.NAME, "pass").send_keys(password)
+
+    def submit_login(self):
+        self.driver.find_element(By.XPATH, "//input[@value='Login']").click()
+
+class GroupHelper:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def create_group(self, group):
+        self.open_groups_page()
+        self.init_group_creation()
+        self.fill_group_form(group)
+        self.submit_group_creation()
+        self.return_to_groups_page()
+
+    def open_groups_page(self):
+        self.driver.find_element(By.LINK_TEXT, "groups").click()
+
+    def init_group_creation(self):
+        self.driver.find_element(By.NAME, "new").click()
+
+    def fill_group_form(self, group):
+        self.driver.find_element(By.NAME, "group_name").clear()
+        self.driver.find_element(By.NAME, "group_name").send_keys(group.name)
+        self.driver.find_element(By.NAME, "group_header").clear()
+        self.driver.find_element(By.NAME, "group_header").send_keys(group.header)
+        self.driver.find_element(By.NAME, "group_footer").clear()
+        self.driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
+
+    def submit_group_creation(self):
+        self.driver.find_element(By.NAME, "submit").click()
+
+    def return_to_groups_page(self):
+        self.driver.find_element(By.LINK_TEXT, "group page").click()
+
+class ContactHelper:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def create_contact(self, contact):
+        pass
+
+    def edit_contact(self, contact):
+        pass
+
+    def delete_contact(self):
+        pass
+
+class Application:
+    def __init__(self, driver):
+        self.driver = driver
+        self.session = SessionHelper(self.driver)
+        self.group = GroupHelper(self.driver)
+        self.contact = ContactHelper(self.driver)
+
 @pytest.fixture(scope="module")
-def driver():
-    service = Service(executable_path="C:\Windows\SysWOW64\geckodriver.exe")
+def app():
+    service = Service(executable_path="C:\\Windows\\SysWOW64\\geckodriver.exe")
     driver = webdriver.Firefox(service=service)
     driver.implicitly_wait(10)
-    yield driver
+    app = Application(driver)
+    yield app
     driver.quit()
 
-@pytest.fixture
-def login(driver):
-    def _login(username, password):
-        open_home_page(driver)
-        fill_login_form(driver, username, password)
-        submit_login(driver)
-    return _login
+def test_create_group(app):
+    app.session.login("admin", "secret")
+    group = Group(name="lozka", header="kolbasa", footer="spati")
+    app.group.create_group(group)
+    app.session.logout()
 
-@pytest.fixture
-def logout(driver):
-    def _logout():
-        driver.find_element(By.LINK_TEXT, "Logout").click()
-    return _logout
-
-@pytest.fixture
-def create_group(driver):
-    def _create_group(group):
-        open_groups_page(driver)
-        init_group_creation(driver)
-        fill_group_form(driver, group)
-        submit_group_creation(driver)
-        return_to_groups_page(driver)
-    return _create_group
-
-def open_home_page(driver):
-    driver.get("http://localhost/addressbook/")
-
-def fill_login_form(driver, username, password):
-    driver.find_element(By.NAME, "user").clear()
-    driver.find_element(By.NAME, "user").send_keys(username)
-    driver.find_element(By.NAME, "pass").clear()
-    driver.find_element(By.NAME, "pass").send_keys(password)
-
-def submit_login(driver):
-    driver.find_element(By.XPATH, "//input[@value='Login']").click()
-
-def open_groups_page(driver):
-    driver.find_element(By.LINK_TEXT, "groups").click()
-
-def init_group_creation(driver):
-    driver.find_element(By.NAME, "new").click()
-
-def fill_group_form(driver, group):
-    driver.find_element(By.NAME, "group_name").clear()
-    driver.find_element(By.NAME, "group_name").send_keys(group.name)
-    driver.find_element(By.NAME, "group_header").clear()
-    driver.find_element(By.NAME, "group_header").send_keys(group.header)
-    driver.find_element(By.NAME, "group_footer").clear()
-    driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
-
-def submit_group_creation(driver):
-    driver.find_element(By.NAME, "submit").click()
-
-def return_to_groups_page(driver):
-    driver.find_element(By.LINK_TEXT, "group page").click()
-
-def test_create_group(driver, login, logout, create_group):
-    driver.get("http://localhost/addressbook/")
-    login("admin", "secret")
-    group = Group("lozka", "kolbasa", "spati")
-    create_group(group)
-    logout()
+def test_create_contact(app):
+    app.session.login("admin", "secret")
+    app.session.logout()
--- test_contact.py
+++ test_contact.py
@@ -3,7 +3,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
-
+from selenium.common.exceptions import NoSuchElementException
 
 class Group:
     def __init__(self, name, header, footer):
@@ -11,92 +11,108 @@ class Group:
         self.header = header
         self.footer = footer
 
+class SessionHelper:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def login(self, username, password):
+        self.open_home_page()
+        self.fill_login_form(username, password)
+        self.submit_login()
+
+    def logout(self):
+        self.driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def open_home_page(self):
+        self.driver.get("http://localhost/addressbook/")
+
+    def fill_login_form(self, username, password):
+        self.driver.find_element(By.NAME, "user").clear()
+        self.driver.find_element(By.NAME, "user").send_keys(username)
+        self.driver.find_element(By.NAME, "pass").clear()
+        self.driver.find_element(By.NAME, "pass").send_keys(password)
+
+    def submit_login(self):
+        self.driver.find_element(By.XPATH, "//input[@value='Login']").click()
+
+class GroupHelper:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def create_group(self, group):
+        self.open_groups_page()
+        self.init_group_creation()
+        self.fill_group_form(group)
+        self.submit_group_creation()
+        self.return_to_groups_page()
+
+    def open_groups_page(self):
+        self.driver.find_element(By.LINK_TEXT, "groups").click()
+
+    def init_group_creation(self):
+        self.driver.find_element(By.NAME, "new").click()
+
+    def fill_group_form(self, group):
+        self.driver.find_element(By.NAME, "group_name").clear()
+        self.driver.find_element(By.NAME, "group_name").send_keys(group.name)
+        self.driver.find_element(By.NAME, "group_header").clear()
+        self.driver.find_element(By.NAME, "group_header").send_keys(group.header)
+        self.driver.find_element(By.NAME, "group_footer").clear()
+        self.driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
+
+    def submit_group_creation(self):
+        self.driver.find_element(By.NAME, "submit").click()
+
+    def return_to_groups_page(self):
+        self.driver.find_element(By.LINK_TEXT, "group page").click()
+
+    def is_group_created(self, group_name):
+        self.open_groups_page()
+        try:
+            self.driver.find_element(By.XPATH, f"//span[text()='{group_name}']")
+            return True
+        except NoSuchElementException:
+            return False
+
+class ContactHelper:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def create_contact(self, contact):
+        pass
+
+    def edit_contact(self, contact):
+        pass
+
+    def delete_contact(self):
+        pass
+
+class Application:
+    def __init__(self, driver):
+        self.driver = driver
+        self.session = SessionHelper(self.driver)
+        self.group = GroupHelper(self.driver)
+        self.contact = ContactHelper(self.driver)
+
 @pytest.fixture(scope="module")
-def driver():
+def app():
     service = Service(executable_path="C:\\Windows\\SysWOW64\\geckodriver.exe")
     driver = webdriver.Firefox(service=service)
     driver.implicitly_wait(10)
-    yield driver
+    app = Application(driver)
+    yield app
     driver.quit()
-
-@pytest.fixture
-def login(driver):
-    def _login(username, password):
-        open_home_page(driver)
-        fill_login_form(driver, username, password)
-        submit_login(driver)
-
-    return _login
-
-@pytest.fixture
-def logout(driver):
-    def _logout():
-        driver.find_element(By.LINK_TEXT, "Logout").click()
-
-    return _logout
-
-@pytest.fixture
-def create_group(driver):
-    def _create_group(group):
-        open_groups_page(driver)
-        init_group_creation(driver)
-        fill_group_form(driver, group)
-        submit_group_creation(driver)
-        return_to_groups_page(driver)
-
-    return _create_group
 
 @pytest.fixture
 def test_group():
     return Group(name="lozka", header="kolbasa", footer="spati")
 
-def open_home_page(driver):
-    driver.get("http://localhost/addressbook/")
+def test_create_group(app, test_group):
+    app.session.login("admin", "secret")
+    app.group.create_group(test_group)
+    assert app.group.is_group_created(test_group.name), "Группа не была создана"
+    app.session.logout()
 
-def fill_login_form(driver, username, password):
-    driver.find_element(By.NAME, "user").clear()
-    driver.find_element(By.NAME, "user").send_keys(username)
-    driver.find_element(By.NAME, "pass").clear()
-    driver.find_element(By.NAME, "pass").send_keys(password)
-
-def submit_login(driver):
-    driver.find_element(By.XPATH, "//input[@value='Login']").click()
-
-def open_groups_page(driver):
-    driver.find_element(By.LINK_TEXT, "groups").click()
-
-def init_group_creation(driver):
-    driver.find_element(By.NAME, "new").click()
-
-def fill_group_form(driver, group):
-    driver.find_element(By.NAME, "group_name").clear()
-    driver.find_element(By.NAME, "group_name").send_keys(group.name)
-    driver.find_element(By.NAME, "group_header").clear()
-    driver.find_element(By.NAME, "group_header").send_keys(group.header)
-    driver.find_element(By.NAME, "group_footer").clear()
-    driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
-
-def submit_group_creation(driver):
-    driver.find_element(By.NAME, "submit").click()
-
-def return_to_groups_page(driver):
-    driver.find_element(By.LINK_TEXT, "group page").click()
-
-def test_create_group(driver, login, logout, create_group, test_group):
-    open_home_page(driver)
-
-    login("admin", "secret")
-
-    create_group(test_group)
-
-    assert is_group_created(driver, test_group.name), "Группа не была создана"
-
-    logout()
-
-def is_group_created(driver, group_name):
-    open_groups_page(driver)
-    try:
-        driver.find_element(By.XPATH, f"//span[text()='{group_name}']")
-        return True
-    except NoSuchElementException:
-        return False
+def test_create_contact(app):
+    app.session.login("admin", "secret")
+    app.session.logout()
