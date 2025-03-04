3988dfa837da76467e6e708e872296f5bf6cf23a
aimitrofanova <aimitrofanova@mail.ru>
2025-03-04 16:10:37 +0600

    project

--- test_add_group.py
+++ test_add_group.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
-import unittest, time, re
 from selenium.webdriver.firefox.service import Service
 
 class Group:
@@ -14,114 +14,73 @@ class Group:
         self.header = header
         self.footer = footer
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        service = Service(executable_path="C:\Windows\SysWOW64\geckodriver.exe")
-        self.driver = webdriver.Firefox(service=service)
-        self.driver.implicitly_wait(10)
-        self.base_url = "http://localhost/addressbook/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+@pytest.fixture(scope="module")
+def driver():
+    service = Service(executable_path="C:\Windows\SysWOW64\geckodriver.exe")
+    driver = webdriver.Firefox(service=service)
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
 
-    def test_untitled_test_case(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/")
+@pytest.fixture
+def login(driver):
+    def _login(username, password):
+        open_home_page(driver)
+        fill_login_form(driver, username, password)
+        submit_login(driver)
+    return _login
 
-        # Логин
-        self.login("admin", "secret")
-        # Создание группы
-        group = Group("lozka", "kolbasa", "spati")
-        self.create_new_group(group)
-        # Логаут
-        self.logout()
-
-    def login(self, username, password):
-         self.open_home_page()
-         self.fill_login_form(username, password)
-         self.submit_login()
-
-    def open_home_page(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/")
-
-    def fill_login_form(self, username, password):
-        driver = self.driver
-        self.driver.find_element(By.NAME, "user").clear()
-        self.driver.find_element(By.NAME, "user").send_keys(username)
-        self.driver.find_element(By.NAME, "pass").clear()
-        self.driver.find_element(By.NAME, "pass").send_keys(password)
-
-    def submit_login(self):
-        driver = self.driver
-        self.driver.find_element(By.XPATH, "//input[@value='Login']").click()
-
-    def logout(self):
-        driver = self.driver
+@pytest.fixture
+def logout(driver):
+    def _logout():
         driver.find_element(By.LINK_TEXT, "Logout").click()
+    return _logout
 
-    def create_new_group(self, group):
-        driver = self.driver
-        self.open_groups_page()
-        self.init_group_creation()
-        self.fill_group_form(group)
-        self.submit_group_creation()
-        self.return_to_groups_page()
-    def open_groups_page(self):
-        driver = self.driver
-        driver.find_element(By.LINK_TEXT, "groups").click()
+@pytest.fixture
+def create_group(driver):
+    def _create_group(group):
+        open_groups_page(driver)
+        init_group_creation(driver)
+        fill_group_form(driver, group)
+        submit_group_creation(driver)
+        return_to_groups_page(driver)
+    return _create_group
 
-    def init_group_creation(self):
-        driver = self.driver
-        driver.find_element(By.NAME, "new").click()
+def open_home_page(driver):
+    driver.get("http://localhost/addressbook/")
 
-    def fill_group_form(self, group):
-        driver = self.driver
-        driver.find_element(By.NAME, "group_name").clear()
-        driver.find_element(By.NAME, "group_name").send_keys(group.name)
-        driver.find_element(By.NAME, "group_header").clear()
-        driver.find_element(By.NAME, "group_header").send_keys(group.header)
-        driver.find_element(By.NAME, "group_footer").clear()
-        driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
+def fill_login_form(driver, username, password):
+    driver.find_element(By.NAME, "user").clear()
+    driver.find_element(By.NAME, "user").send_keys(username)
+    driver.find_element(By.NAME, "pass").clear()
+    driver.find_element(By.NAME, "pass").send_keys(password)
 
-    def submit_group_creation(self):
-        driver = self.driver
-        driver.find_element(By.NAME, "submit").click()
+def submit_login(driver):
+    driver.find_element(By.XPATH, "//input[@value='Login']").click()
 
-    def return_to_groups_page(self):
-        driver = self.driver
-        driver.find_element(By.LINK_TEXT, "group page").click()
+def open_groups_page(driver):
+    driver.find_element(By.LINK_TEXT, "groups").click()
 
+def init_group_creation(driver):
+    driver.find_element(By.NAME, "new").click()
 
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
+def fill_group_form(driver, group):
+    driver.find_element(By.NAME, "group_name").clear()
+    driver.find_element(By.NAME, "group_name").send_keys(group.name)
+    driver.find_element(By.NAME, "group_header").clear()
+    driver.find_element(By.NAME, "group_header").send_keys(group.header)
+    driver.find_element(By.NAME, "group_footer").clear()
+    driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
 
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to.alert
-        except NoAlertPresentException as e:
-            return False
-        return True
+def submit_group_creation(driver):
+    driver.find_element(By.NAME, "submit").click()
 
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to.alert
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
+def return_to_groups_page(driver):
+    driver.find_element(By.LINK_TEXT, "group page").click()
 
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_create_group(driver, login, logout, create_group):
+    driver.get("http://localhost/addressbook/")
+    login("admin", "secret")
+    group = Group("lozka", "kolbasa", "spati")
+    create_group(group)
+    logout()
--- test_contact.py
+++ test_contact.py
@@ -1,133 +1,102 @@
 # -*- coding: utf-8 -*-
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
 from selenium.webdriver.firefox.service import Service
 
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        service = Service(executable_path="C:\Windows\SysWOW64\geckodriver.exe")
-        self.driver = webdriver.Firefox(service=service)
-        self.driver.implicitly_wait(10)
-        self.base_url = "http://localhost/addressbook/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+class Group:
+    def __init__(self, name, header, footer):
+        self.name = name
+        self.header = header
+        self.footer = footer
 
-    def test_untitled_test_case(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/edit.php")
-        self.login("admin", "secret")
-        self.create_contact(
-            "Alina",
-            "Mitrofanova",
-            "painkiller",
-            "ergr",
-            "rgreg",
-            "hty",
-            "weew",
-            "34343546",
-            "ffeewe",
-            "nyjt4",
-            "egrhtjkjythrew@GRGR.RU",
-            "14",
-            "July",
-            "2001",
-        )
-        self.return_to_home_page()
+@pytest.fixture(scope="module")
+def driver():
+    service = Service(executable_path="C:\\Windows\\SysWOW64\\geckodriver.exe")
+    driver = webdriver.Firefox(service=service)
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
 
-    def login(self, username, password):
-        driver = self.driver
-        driver.find_element(By.NAME, "user").clear()
-        driver.find_element(By.NAME, "user").send_keys(username)
-        driver.find_element(By.NAME, "pass").clear()
-        driver.find_element(By.NAME, "pass").send_keys(password)
-        driver.find_element(By.XPATH, "//input[@value='Login']").click()
+@pytest.fixture
+def login(driver):
+    def _login(username, password):
+        open_home_page(driver)
+        fill_login_form(driver, username, password)
+        submit_login(driver)
 
-    def create_contact(
-        self,
-        firstname,
-        middlename,
-        nickname,
-        title,
-        company,
-        address,
-        homephone,
-        mobilephone,
-        workphone,
-        fax,
-        email,
-        bday,
-        bmonth,
-        byear,
-    ):
-        driver = self.driver
-        driver.find_element(By.LINK_TEXT, "add new").click()
-        driver.find_element(By.NAME, "firstname").clear()
-        driver.find_element(By.NAME, "firstname").send_keys(firstname)
-        driver.find_element(By.NAME, "middlename").clear()
-        driver.find_element(By.NAME, "middlename").send_keys(middlename)
-        driver.find_element(By.NAME, "nickname").clear()
-        driver.find_element(By.NAME, "nickname").send_keys(nickname)
-        driver.find_element(By.NAME, "title").clear()
-        driver.find_element(By.NAME, "title").send_keys(title)
-        driver.find_element(By.NAME, "company").clear()
-        driver.find_element(By.NAME, "company").send_keys(company)
-        driver.find_element(By.NAME, "address").clear()
-        driver.find_element(By.NAME, "address").send_keys(address)
-        driver.find_element(By.NAME, "home").clear()
-        driver.find_element(By.NAME, "home").send_keys(homephone)
-        driver.find_element(By.NAME, "mobile").clear()
-        driver.find_element(By.NAME, "mobile").send_keys(mobilephone)
-        driver.find_element(By.NAME, "work").clear()
-        driver.find_element(By.NAME, "work").send_keys(workphone)
-        driver.find_element(By.NAME, "fax").clear()
-        driver.find_element(By.NAME, "fax").send_keys(fax)
-        driver.find_element(By.NAME, "email").clear()
-        driver.find_element(By.NAME, "email").send_keys(email)
-        Select(driver.find_element(By.NAME, "bday")).select_by_visible_text(bday)
-        Select(driver.find_element(By.NAME, "bmonth")).select_by_visible_text(bmonth)
-        driver.find_element(By.NAME, "byear").clear()
-        driver.find_element(By.NAME, "byear").send_keys(byear)
-        driver.find_element(By.XPATH, "//input[@value='Enter']").click()
+    return _login
 
-    def return_to_home_page(self):
-        driver = self.driver
-        driver.find_element(By.LINK_TEXT, "home page").click()
+@pytest.fixture
+def logout(driver):
+    def _logout():
+        driver.find_element(By.LINK_TEXT, "Logout").click()
 
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(how, what)
-        except NoSuchElementException as e:
-            return False
+    return _logout
+
+@pytest.fixture
+def create_group(driver):
+    def _create_group(group):
+        open_groups_page(driver)
+        init_group_creation(driver)
+        fill_group_form(driver, group)
+        submit_group_creation(driver)
+        return_to_groups_page(driver)
+
+    return _create_group
+
+@pytest.fixture
+def test_group():
+    return Group(name="lozka", header="kolbasa", footer="spati")
+
+def open_home_page(driver):
+    driver.get("http://localhost/addressbook/")
+
+def fill_login_form(driver, username, password):
+    driver.find_element(By.NAME, "user").clear()
+    driver.find_element(By.NAME, "user").send_keys(username)
+    driver.find_element(By.NAME, "pass").clear()
+    driver.find_element(By.NAME, "pass").send_keys(password)
+
+def submit_login(driver):
+    driver.find_element(By.XPATH, "//input[@value='Login']").click()
+
+def open_groups_page(driver):
+    driver.find_element(By.LINK_TEXT, "groups").click()
+
+def init_group_creation(driver):
+    driver.find_element(By.NAME, "new").click()
+
+def fill_group_form(driver, group):
+    driver.find_element(By.NAME, "group_name").clear()
+    driver.find_element(By.NAME, "group_name").send_keys(group.name)
+    driver.find_element(By.NAME, "group_header").clear()
+    driver.find_element(By.NAME, "group_header").send_keys(group.header)
+    driver.find_element(By.NAME, "group_footer").clear()
+    driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
+
+def submit_group_creation(driver):
+    driver.find_element(By.NAME, "submit").click()
+
+def return_to_groups_page(driver):
+    driver.find_element(By.LINK_TEXT, "group page").click()
+
+def test_create_group(driver, login, logout, create_group, test_group):
+    open_home_page(driver)
+
+    login("admin", "secret")
+
+    create_group(test_group)
+
+    assert is_group_created(driver, test_group.name), "Группа не была создана"
+
+    logout()
+
+def is_group_created(driver, group_name):
+    open_groups_page(driver)
+    try:
+        driver.find_element(By.XPATH, f"//span[text()='{group_name}']")
         return True
-
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to.alert
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to.alert
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
-
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
-    if __name__ == "__main__":
-        unittest.main()
+    except NoSuchElementException:
+        return False
